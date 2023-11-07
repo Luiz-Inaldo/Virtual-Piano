30ab2a318acec221c900f8630266de87535cd3c3
Luiz-Inaldo <luiz.inaldo970@gmail.com>
2023-11-06 21:38:18 -0300

    adicionando as teclas ao piano

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,10 +3,15 @@ const pianoKeys = document.querySelectorAll(".piano-keys .key");
 let audio = new Audio('src/tunes/a.wav');
 
 function playTune(key) {
-    audio.src = `src/tunes/${key}.wav`;
+    audio.src = `src/tunes/${key}.wav`; // indicando o diretório de cada tecla clicada/pressionada.
     audio.play(key);
 }
 
 pianoKeys.forEach((key) => {
-    key.addEventListener('click', () => playTune(key.dataset.key));
+    console.log(key.dataset.key);
+    // criando um evento para cada tecla que será ativado quando for clicada.
+    key.addEventListener('click', () => playTune(key.dataset.key)); // dataset vai puxar no HTML os seletores que contem o atributo data-.
 });
+
+// acionando o teclado para usar no piano.
+document.addEventListener('keydown', (evento) => playTune(evento.key));
